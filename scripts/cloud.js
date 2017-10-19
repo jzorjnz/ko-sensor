a8e139eb648c2fe3686c90b971eb4a105641f8e8
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2017-10-19 16:09:34 +0200

    Fixed the graph animations. Added logo
    Some other bug fixes.

--- scripts/cloud.js
+++ scripts/cloud.js
@@ -1,11 +1,19 @@
 function Cloud() {
     self = this;
     this.mbed_server = "https://api.connector.mbed.com/v2";
-    //this.notification_server = "http://93.62.59.68:53576";
+    
+    //this.notification_server = "http://18.221.102.101:53576";
+    
     this.notification_server = "http://192.168.1.20:8082";
+    this.notification_server_out = "http://93.62.59.68:53576";
+    this.notification_path = "/notification";
+
+    //this.notification_server = "http://13.59.209.66:53576";
+    
     //this.key = "KGC48Y04jRsJgYG4Uwnx1dmMfhVImYwy49sc87PafmJDJLdmvexESLhy4J0AeLmbwvbE5kMfIoy9PDSYVoTSvl55Kue3InElCB7N";
     // new
-    this.key = "hPDJSltBhMi9l73Lf1I6S9lXKnIFrVIPfRnwJvq8Ak88AKiY2J87q4nUG2RzOfKd0qAbgd53Iq65Op2qPewv06JssDPJMUsa14mn";
+    //this.key = "hPDJSltBhMi9l73Lf1I6S9lXKnIFrVIPfRnwJvq8Ak88AKiY2J87q4nUG2RzOfKd0qAbgd53Iq65Op2qPewv06JssDPJMUsa14mn";
+    this.key = "P6JGPZM7Oa0pZRsJ8bhHLCHU1kjygeFSzmyeEZmKn5OMfak3Ichsms9aKH6VllncYCNMyqBLkhweMwg2FExZBxZuo2fRU6QOTQcY";
 
     this.authorization = "Bearer " + this.key;
     this.headers = {
@@ -14,8 +22,7 @@ function Cloud() {
     }
 }
 
-Cloud.prototype.getDevices = function (packet) {
-    var self = this;
+Cloud.prototype.getEndpoints = function () {
     return new Promise(function (resolve, reject) {
       $.ajax({
         url: self.mbed_server + "/endpoints",
@@ -30,8 +37,6 @@ Cloud.prototype.getDevices = function (packet) {
   };
 
 Cloud.prototype.getEndpointInfo = function (endpoint) {
-    var self = this;
-    //console.log("packet: " + JSON.stringify(packet));
     return new Promise(function (resolve, reject) {
       $.ajax({
         url: self.mbed_server + "/endpoints/" + endpoint,
@@ -46,8 +51,7 @@ Cloud.prototype.getEndpointInfo = function (endpoint) {
   };
 
 
-Cloud.prototype.sendNotificationRequest = function (endpoint) {
-    //console.log("packet: " + JSON.stringify(packet));
+Cloud.prototype.registerNotificationChannel = function (endpoint) {
     return new Promise(function (resolve, reject) {
       $.ajax({
         url: self.mbed_server + "/notification/callback",
@@ -55,10 +59,9 @@ Cloud.prototype.sendNotificationRequest = function (endpoint) {
         contentType: "application/json",
         data: JSON.stringify(
           {
-            "url": self.notification_server +  "/data",
+            "url": (self.notification_server_out ?  self.notification_server_out : self.notification_server) +  self.notification_path,
             "headers" : {
-              "Authorization" : "auth", 
-              "test-header" : "test"
+              "Authorization" : "auth"
             }
           }),
         headers: self.headers,
@@ -71,8 +74,6 @@ Cloud.prototype.sendNotificationRequest = function (endpoint) {
 
 
   Cloud.prototype.startNotificationService = function (params) {
-    //console.log("packet: " + JSON.stringify(packet));
-    console.log('starting...');
     return new Promise(function (resolve, reject) {
       $.ajax({
         url: self.notification_server +  "/start/" + params.endpoint + "/" + params.path,
@@ -86,8 +87,6 @@ Cloud.prototype.sendNotificationRequest = function (endpoint) {
   };
 
   Cloud.prototype.stopNotificationService = function (params) {
-    //console.log("packet: " + JSON.stringify(packet));
-    console.log('stopping...');
     return new Promise(function (resolve, reject) {
       $.ajax({
         url: self.notification_server +  "/stop/" + params.endpoint + "/" + params.path,
@@ -101,8 +100,6 @@ Cloud.prototype.sendNotificationRequest = function (endpoint) {
   };
 
   Cloud.prototype.getResource = function (params) {
-    //console.log("packet: " + JSON.stringify(packet));
-    console.log('gettting resources...');
     return new Promise(function (resolve, reject) {
       $.ajax({
         url: self.notification_server +  "/get_resource/" + params.endpoint + "/" + params.path,
@@ -115,5 +112,17 @@ Cloud.prototype.sendNotificationRequest = function (endpoint) {
     });
   };
 
+  Cloud.prototype.getAllResources = function (params) {
+    return new Promise(function (resolve, reject) {
+      $.ajax({
+        url: self.notification_server +  "/get_all_resources/" + params.endpoint,
+        type: "get",
+        contentType: "application/json"
+      }).done(function (result) {
+        console.log(result);
+        resolve(result);
+      });
+    });
+  };
   
 var cloud = new Cloud(); 
--- scripts/vm.js
+++ scripts/vm.js
@@ -4,17 +4,33 @@ function KoModel(data) {
   self.selectedEndpoint = ko.observable();
   self.availableEndpoints = ko.observableArray ([]);
   
+  self.selectedEndpoint.subscribe(function(newValue) {
+    console.log('changed to' + newValue);
+    self.changeButtonsVisibility();
+  }, self);
+
   self.resourceTemp = "3303/0/5700";
   self.resourcePressure = "3300/0/5700";
+  self.resourceHumidity = "3304/0/5700";
   self.resourceButton = "3200/0/5501";
 
   self.tempData = ko.observable({labels: [], datasets: [{ label: "Temperature Dataset", fillColor: "rgba(220,220,220,0.2)", strokeColor: "rgba(220,220,220,1)", pointColor: "rgba(220,220,220,1)", pointStrokeColor: "#fff", pointHighlightFill: "#fff", pointHighlightStroke: "rgba(220,220,220,1)",data: []}]});
   self.pressureData = ko.observable({labels: [], datasets: [{ label: "Pressure Dataset", fillColor: "rgba(220,220,220,0.2)", strokeColor: "rgba(220,220,220,1)", pointColor: "rgba(220,220,220,1)", pointStrokeColor: "#fff", pointHighlightFill: "#fff", pointHighlightStroke: "rgba(220,220,220,1)",data: []}]});
+  self.humidityData = ko.observable({labels: [], datasets: [{ label: "Humidity Dataset", fillColor: "rgba(220,220,220,0.2)", strokeColor: "rgba(220,220,220,1)", pointColor: "rgba(220,220,220,1)", pointStrokeColor: "#fff", pointHighlightFill: "#fff", pointHighlightStroke: "rgba(220,220,220,1)",data: []}]});
   self.buttonData = ko.observable({labels: [], datasets: [{ label: "Button Dataset", fillColor: "rgba(220,220,220,0.2)", strokeColor: "rgba(220,220,220,1)", pointColor: "rgba(220,220,220,1)", pointStrokeColor: "#fff", pointHighlightFill: "#fff", pointHighlightStroke: "rgba(220,220,220,1)",data: []}]});
   
+  self.isServiceRunning = {};
+
+  self.testData = ko.observableArray ([]);
+  self.startButton;
+  self.stopButton;
+
+  self.startButton = document.getElementById('start-button');
+  self.stopButton = document.getElementById('stop-button');
+
   
   self.getEndpoints = function() {
-    cloud.getDevices().then(function (data) {
+    cloud.getEndpoints().then(function (data) {
       console.log("function response: " + JSON.stringify(data));
       self.availableEndpoints.removeAll();
       for (var key in data) {
@@ -28,7 +44,20 @@ function KoModel(data) {
   self.getEndpointInfo = function() {
     if(self.selectedEndpoint() && self.selectedEndpoint().length > 0 ){
       console.log("" + self.selectedEndpoint());
-      cloud.sendNotificationRequest("" + self.selectedEndpoint()).then(function (data) {
+      cloud.getEndpointInfo("" + self.selectedEndpoint()).then(function (data) {
+        console.log("function response: " + JSON.stringify(data));
+        
+      });
+    }
+    else{
+      alert("Please select endpoint first!");
+    }
+  };
+  
+  self.registerNotificationChannel = function() {
+    if(self.selectedEndpoint() && self.selectedEndpoint().length > 0 ){
+      console.log("" + self.selectedEndpoint());
+      cloud.registerNotificationChannel().then(function (data) {
         console.log("function response: " + JSON.stringify(data));
         
       });
@@ -39,15 +68,16 @@ function KoModel(data) {
     
   };
   
+  
   self.startNotificationService = function(path) {
     if(self.selectedEndpoint() && self.selectedEndpoint().length > 0 ){
       params = {
         "endpoint": self.selectedEndpoint(),
-        "path": path//"3303/0/5700" //3200/0/5501"
+        "path": path
       }
       cloud.startNotificationService(params).then(function (data) {
-        console.log("function response: " + JSON.stringify(data));
-        self.getResource(path);
+        // To get resources as soon as service for the resource has started
+        //self.getResource(path);
       });
     }
     else{
@@ -59,7 +89,7 @@ function KoModel(data) {
     if(self.selectedEndpoint() && self.selectedEndpoint().length > 0 ){
       params = {
         "endpoint": self.selectedEndpoint(),
-        "path": path//"3303/0/5700" //3200/0/5501"
+        "path": path
       }
       cloud.stopNotificationService(params).then(function (data) {
         console.log("function response: " + JSON.stringify(data));
@@ -70,78 +100,117 @@ function KoModel(data) {
     }
   };
 
+  self.parseResource = function(data, path){
+    values = [];
+    var labels = [];
+    var i = 1;
+    self.testData.removeAll();
+    for(var key in data){
+      if(data.hasOwnProperty(key)){
+        d = new Date(data[key].time);
+        
+        labels.push(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+        values.push(data[key].payload);
+        self.testData.push(data[key].payload);
+        i++;
+      }
+    }
+    
+    if(path == self.resourceTemp){
+      console.log('showing for temp!');
+      self.tempData({
+        labels: labels,
+        datasets: [{
+          label: "Button resource dataset",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: values
+        }]
+      });
+    }
+    else if(path == self.resourcePressure){
+      console.log('showing for pressure!');
+      self.pressureData({
+        labels: labels,
+        datasets: [{
+          label: "Button resource dataset",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: values
+        }]
+      });
+    }
+    else if(path == self.resourceButton){
+      console.log('showing for button!');
+      self.buttonData({
+        labels: labels,
+        datasets: [{
+          label: "Button resource dataset",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: values
+        }]
+      });
+    }
+    else if(path == self.resourceHumidity){
+      console.log('showing for humidity!');
+      self.humidityData({
+        labels: labels,
+        datasets: [{
+          label: "Button resource dataset",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: values
+        }]
+      });
+    }
+  };
+
   self.getResource = function(path) {
     params = {
       endpoint: self.selectedEndpoint(),
       path: path
     }
     cloud.getResource(params).then(function (data) {
-      console.log("function response: " + JSON.stringify(data));
-      
-      var values = [];
-      var labels = [];
-      var i = 1;
+      //console.log("function response: " + JSON.stringify(data));
+      self.parseResource(data, path);
+    });
+    if(self.isServiceRunning[self.selectedEndpoint()]){
+      setTimeout(self.getResource, 1000, path);
+    }
+  };
+
+  self.getAllResources = function() {
+    params = {
+      endpoint: self.selectedEndpoint()
+    }
+    cloud.getAllResources(params).then(function (data) {
+      //console.log("function response: " + JSON.stringify(data));
       for(var key in data){
         if(data.hasOwnProperty(key)){
-          d = new Date(data[key].time);
-          
-          labels.push(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
-          values.push(data[key].payload);
-          i++;
+          self.parseResource(data[key], key.substring(1));
         }
       }
-      
-      if(path == self.resourceTemp){
-        console.log('showing for temp!');
-        self.tempData({
-          labels: labels,
-          datasets: [{
-            label: "Button resource dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: values
-          }]
-        });
-      }
-      else if(path == self.resourcePressure){
-        console.log('showing for pressure!');
-        self.pressureData({
-          labels: labels,
-          datasets: [{
-            label: "Button resource dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: values
-          }]
-        });
-      }
-      else if(path == self.resourceButton){
-        console.log('showing for button!');
-        self.buttonData({
-          labels: labels,
-          datasets: [{
-            label: "Button resource dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: values
-          }]
-        });
-      }
-      
     });
-    setTimeout(self.getResource, 1000, path);
+    if(self.isServiceRunning[self.selectedEndpoint()]){
+      setTimeout(self.getAllResources, 1000);
+    }
   };
 
   self.startTempNotificationService = function() {
@@ -181,15 +250,46 @@ function KoModel(data) {
   };
   
   self.startService = function(){
+    self.isServiceRunning[self.selectedEndpoint()] = true;
     self.startNotificationService(self.resourceTemp);
     self.startNotificationService(self.resourcePressure);
+    self.startNotificationService(self.resourceHumidity);
     self.startNotificationService(self.resourceButton);
+    self.getAllResources();
+    self.changeButtonsVisibility();
   };
 
+  self.stopService = function(){
+    self.isServiceRunning[self.selectedEndpoint()] = false;
+    self.stopNotificationService(self.resourceTemp);
+    self.stopNotificationService(self.resourcePressure);
+    self.stopNotificationService(self.resourceHumidity);
+    self.stopNotificationService(self.resourceButton);
+    self.changeButtonsVisibility();
+  };
+
+  self.changeButtonsVisibility = function(){
+    if(self.isServiceRunning[self.selectedEndpoint()]){
+      self.startButton.style.visibility = "hidden";
+      self.stopButton.style.visibility = "visible";
+    }
+    else{
+      self.startButton.style.visibility = "visible";
+      self.stopButton.style.visibility = "hidden";
+    }
+  };
+  /*
+  self.changeButtonsVisibility = function(obj, event){
+    console.log('yayay!');
+    self.changeButtonsVisibility();
+  };
+  */
 };
 
 $(document).ready(function() {
   var data = {};
   var koModel = new KoModel(data);
   ko.applyBindings(koModel);
+
+  document.getElementById('stop-button').style.visibility = "hidden";
 });
